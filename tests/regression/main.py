346cbdfd6ac155d0eb87d57718e902667bd2915f
Anil <anil_makineni@mednax.com>
2023-06-22 16:34:40 -0400

    Add account resource regression tests

--- tests/regression/main.py
+++ tests/regression/main.py
@@ -3,11 +3,13 @@ import sys
 from tests.regression.patient_resource import patient_test
 from tests.regression.organization_resource import org_test
 from tests.regression.encounter_resource import enc_test
+from tests.regression.account_resource import acc_test
 
 def run_tests():
     patient_test()
     org_test()
     enc_test()
+    acc_test()
 
 def cli(args=None):
     """Process command line arguments."""
