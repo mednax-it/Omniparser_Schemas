55841ecf75e182360b617932b4b7841f759d4cda
Anil <anil_makineni@mednax.com>
2023-06-26 15:18:40 -0400

    run ETL parser per resource per test file

--- src/omniparser_schemas/parser/main.py
+++ src/omniparser_schemas/parser/main.py
@@ -4,8 +4,8 @@ import sys
 
 from omniparser_schemas.parser.universal_etl_parser import fetch_parsed_text
 
-def validate_ETL_parser():
-   with importlib.resources.path("omniparser_schemas.parser", "RegressionTestData.txt") as f:
+def validate_ETL_parser(resource, file):
+   with importlib.resources.path("tests.regression.regression_test_data." + str(resource), str(resource) + "_test_data_" + str(file) + ".txt") as f:
       hl7content = f.read_text()
    # The universal parser need to have carriage return
    hl7content = hl7content.replace('\\r', '\r')
@@ -13,12 +13,12 @@ def validate_ETL_parser():
    # call universal utl parser
    fhir_bundle_from_hl7 = fetch_parsed_text(hl7content)
       # Remove first and last character of fhir bundle
-   
+
    fhir_bundle = fhir_bundle_from_hl7[1:-1]
 
    json_data = json.loads(fhir_bundle)
 
-   with open("src/omniparser_schemas/parser/hl7_regression.json", "w") as f:
+   with open("tests/regression/regression_output/" + str(resource) + "/hl7_regression_" + str(resource) + "_" + str(file) + ".json", "w") as f:
        json.dump(json_data, f)
 
 def cli(args=None):
