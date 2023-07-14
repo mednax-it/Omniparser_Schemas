608c01495ad36c1520741dca1cb47124bffbff61
Anil <anil_makineni@mednax.com>
2023-07-14 11:39:39 -0400

    change the config to run on feature branch only when not in main

--- load_external_variables.sh
+++ load_external_variables.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-process.env.PARSER_URL = (process.env.CIRCLE_BRANCH === 'main')
+process.env.PARSER_URL = ( process.env.CIRCLE_BRANCH === 'main' )
     ? process.env.DEV_PARSER_URL,
     : process.env.PROD_PARSER_URL;
