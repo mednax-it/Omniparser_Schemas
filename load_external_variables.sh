337aa8d2930ff829be62a7282686e29334f0c400
Anil <anil_makineni@mednax.com>
2023-07-14 14:48:31 -0400

    remove unnecessary lines

--- load_external_variables.sh
+++ load_external_variables.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-declare DEV_PARSER_URL=DEV_PARSER_URL_$1
-declare PROD_PARSER_URL=PROD_PARSER_URL_$1
-
+declare PARSER_URL=PARSER_URL_$1
 
 #!/bin/bash
-
-if [ "${CIRCLE_BRANCH}" == "main" ]
-then
-  echo "export PARSER_URL=$"${PROD_PARSER_URL} >> $BASH_ENV
-  echo "loaded prod env variables"
-else
-  echo "export PARSER_URL=$"${DEV_PARSER_URL} >> $BASH_ENV
-  echo "looaded preprod env variables"
-fi
-
-echo "load env variables in the project"
+echo "export PARSER_URL=$"${PARSER_URL} >> $BASH_ENV
 
 exit 0
