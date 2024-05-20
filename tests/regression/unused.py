5f01017a7717c8a00a47f396392d7deceb363999
Anil <anil_makineni@mednax.com>
2024-05-20 17:09:45 -0400

    SMILECDR-886-Make-use-of-Parser

--- tests/regression/unused.py
+++ tests/regression/unused.py
@@ -4,7 +4,7 @@ from omniparser_schemas.parser.main import validate_ETL_parser
 
 
 def unused_test(resource_name, testfile, resource_type):
-    # validate_ETL_parser(resource_name, testfile)
+    validate_ETL_parser(resource_name, testfile)
     with open("tests/regression/regression_output/" + str(resource_name) + "/regression_" + str(resource_name) + "_" + str(testfile) + ".json",  'r') as json_file:
         fhir_bundle = json.load(json_file)
         TARGET_RESOURCE_TYPE = resource_type
